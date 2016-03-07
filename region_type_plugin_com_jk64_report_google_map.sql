e353de3756bb9b26f5819565955e8b591a92c269
JOSHUA\Jeffrey <jk64>
2016-03-07 23:23:31 +0800

    v0.3 - add userpin and distance circle

--- region_type_plugin_com_jk64_report_google_map.sql
+++ region_type_plugin_com_jk64_report_google_map.sql
@@ -32,7 +32,7 @@ wwv_flow_api.create_plugin(
 ,p_plugin_type=>'REGION TYPE'
 ,p_name=>'COM.JK64.REPORT_GOOGLE_MAP'
 ,p_display_name=>'JK64 Report Google Map'
-,p_supported_ui_types=>'DESKTOP'
+,p_supported_ui_types=>'DESKTOP:JQM_SMARTPHONE'
 ,p_javascript_file_urls=>'https://maps.googleapis.com/maps/api/js'
 ,p_plsql_code=>wwv_flow_utilities.join(wwv_flow_t_varchar2(
 'function render_map (',
@@ -61,8 +61,20 @@ wwv_flow_api.create_plugin(
 '',
 '    -- Component attributes',
 '    l_map_height    plugin_attr := p_region.attribute_01;',
-'    l_item_name     plugin_attr := p_region.attribute_02;',
+'    l_id_item       plugin_attr := p_region.attribute_02;',
 '    l_click_zoom    plugin_attr := p_region.attribute_03;    ',
+'    l_sync_item     plugin_attr := p_region.attribute_04;',
+'    l_markericon    plugin_attr := p_region.attribute_05;',
+'    l_latlong       plugin_attr := p_region.attribute_06;',
+'    l_dist_item     plugin_attr := p_region.attribute_07;',
+'    ',
+'    PROCEDURE set_map_extents (p_lat IN NUMBER, p_lng IN NUMBER) IS',
+'    BEGIN',
+'        l_lat_min := LEAST   (NVL(l_lat_min, p_lat), p_lat);',
+'        l_lat_max := GREATEST(NVL(l_lat_max, p_lat), p_lat);',
+'        l_lng_min := LEAST   (NVL(l_lng_min, p_lng), p_lng);',
+'        l_lng_max := GREATEST(NVL(l_lng_max, p_lng), p_lng);',
+'    END set_map_extents;',
 'BEGIN',
 '    -- debug information will be included',
 '    if apex_application.g_debug then',
@@ -71,7 +83,7 @@ wwv_flow_api.create_plugin(
 '            p_region => p_region,',
 '            p_is_printer_friendly => p_is_printer_friendly);',
 '    end if;',
-'    ',
+'',
 '    IF p_region.source IS NOT NULL THEN',
 '',
 '        l_column_value_list := apex_plugin_util.get_data',
@@ -103,21 +115,38 @@ wwv_flow_api.create_plugin(
 '              || '',icon:'' || APEX_ESCAPE.js_literal(l_icon)',
 '              || ''}'';',
 '        ',
-'            l_lat_min := LEAST   (NVL(l_lat_min, l_lat), l_lat);',
-'            l_lat_max := GREATEST(NVL(l_lat_max, l_lat), l_lat);',
-'            l_lng_min := LEAST   (NVL(l_lng_min, l_lng), l_lng);',
-'            l_lng_max := GREATEST(NVL(l_lng_max, l_lng), l_lng);',
+'            set_map_extents (p_lat => l_lat, p_lng => l_lng);',
 '          ',
 '        END LOOP;',
 '        ',
 '    END IF;',
 '    ',
+'    l_lat := NULL;',
+'    l_lng := NULL;',
+'    ',
+'    -- if sync item is set, include its position in the initial map extent',
+'    IF l_sync_item IS NOT NULL THEN',
+'      l_latlong := NVL(v(l_sync_item),l_latlong);',
+'    END IF;',
+'    ',
+'    IF l_latlong IS NOT NULL THEN',
+'      l_lat := TO_NUMBER(SUBSTR(l_latlong,1,INSTR(l_latlong,'','')-1));',
+'      l_lng := TO_NUMBER(SUBSTR(l_latlong,INSTR(l_latlong,'','')+1));',
+'    END IF;',
+'    ',
+'    IF l_lat IS NOT NULL THEN',
+'      set_map_extents (p_lat => l_lat, p_lng => l_lng);',
+'',
 '    -- show entire map if no points to show',
-'    IF l_markers_data IS NULL THEN',
+'    ELSIF l_markers_data IS NULL THEN',
+'      l_lat := 0;',
+'      l_lng := 0;',
+'      l_latlong := ''0,0'';',
 '      l_lat_min := -90;',
 '      l_lat_max := 90;',
 '      l_lng_min := -180;',
 '      l_lng_max := 180;',
+'',
 '    END IF;',
 '    ',
 '    l_southwest := ''{lat:''||TO_CHAR(l_lat_min)||'',lng:''||TO_CHAR(l_lng_min)||''}'';',
@@ -125,16 +154,16 @@ wwv_flow_api.create_plugin(
 '    ',
 '    l_html := q''[',
 '<script>',
-'var map, iw, marker, mapdata;',
-'function addMarker(map,pData) {',
-'  marker = new google.maps.Marker({',
-'                map: map,',
-'                position: new google.maps.LatLng(pData.lat, pData.lng),',
-'                title: pData.name,',
-'                icon: pData.icon',
-'            });',
-' ',
-'  google.maps.event.addListener(marker, ''click'', function () {',
+'var map, iw, reppin, userpin, distcircle, mapdata;',
+'function repPin(map,pData) {',
+'  var reppin = new google.maps.Marker({',
+'                 map: map,',
+'                 position: new google.maps.LatLng(pData.lat, pData.lng),',
+'                 title: pData.name,',
+'                 icon: pData.icon',
+'               }); ',
+'  google.maps.event.addListener(reppin, ''click'', function () {',
+'    console.log(''reppin clicked ''+pData.id);',
 '    if (iw) {',
 '      iw.close();',
 '    } else {',
@@ -148,22 +177,122 @@ wwv_flow_api.create_plugin(
 '    if ("#CLICKZOOM#" != "") {',
 '      map.setZoom(#CLICKZOOM#);',
 '    }',
-'    if ("#ITEMNAME#" !== "") {',
-'      $s("#ITEMNAME#",pData.id);',
+'    if ("#IDITEM#" !== "") {',
+'      $s("#IDITEM#",pData.id);',
 '    }',
-'    apex.jQuery("##REGION_ID#").trigger("mapclick", {id:pData.id, name:pData.name, lat:pData.lat, lng:pData.lng});',
+'    apex.jQuery("##REGION_ID#").trigger("markerclick", {id:pData.id, name:pData.name, lat:pData.lat, lng:pData.lng});',
 '  });',
 '}',
+'function setCircle(map,pos) {',
+'  var distitem = "#DISTITEM#";',
+'  if (distitem!=="") {',
+'    if (distcircle) {',
+'      console.log(''move circle'');',
+'      distcircle.setCenter(pos);',
+'    } else {',
+'      console.log(''create circle'');',
+'      var radius_metres = parseFloat($v(distitem))*1000;',
+'      distcircle = new google.maps.Circle({',
+'          strokeColor: ''#5050FF'',',
+'          strokeOpacity: 0.5,',
+'          strokeWeight: 2,',
+'          fillColor: ''#0000FF'',',
+'          fillOpacity: 0.05,',
+'          clickable: false,',
+'          editable: true,',
+'          map: map,',
+'          center: pos,',
+'          radius: radius_metres',
+'        });',
+'      google.maps.event.addListener(distcircle, ''radius_changed'', function (event) {',
+'        var km = distcircle.getRadius()/1000;',
+'        console.log(''circle radius changed ''+km);',
+'        $s("#DISTITEM#", km);',
+'      });',
+'    }',
+'  }',
+'}',
+'function userPin(map,lat,lng) {',
+'  if (lat !== null && lng !== null) {',
+'    var oldpos = userpin?userpin.getPosition():new google.maps.LatLng(0,0);',
+'    if (lat == oldpos.lat() && lng == oldpos.lng()) {',
+'      console.log(''not changed'');',
+'    } else {',
+'      var pos = new google.maps.LatLng(lat,lng);',
+'      if (userpin) {',
+'        console.log(''move existing pin to new position on map ''+lat+'',''+lng);',
+'        userpin.setMap(map);',
+'        userpin.setPosition(pos);',
+'        setCircle(map,pos);',
+'      } else {',
+'        console.log(''create userpin ''+lat+'',''+lng);',
+'        userpin = new google.maps.Marker({map: map, position: pos, icon: "#ICON#"});',
+'        setCircle(map,pos);',
+'      }',
+'    }',
+'  } else if (userpin) {',
+'    console.log(''move existing pin off the map'');',
+'    userpin.setMap(null);',
+'    if (distcircle) {',
+'      distcircle.setMap(null);',
+'    }',
+'  }',
+'}',
 'function initMap() {',
-'  var bounds = new google.maps.LatLngBounds(#SOUTHWEST#,#NORTHEAST#);',
+'  console.log(''initMap'');',
+'  var syncitem = "#SYNCITEM#"',
+'     ,distitem = "#DISTITEM#"',
+'     ,bounds = new google.maps.LatLngBounds(#SOUTHWEST#,#NORTHEAST#);',
 '  var myOptions = {',
+'    zoom: 1,',
+'    center: new google.maps.LatLng(#LATLNG#),',
 '    mapTypeId: google.maps.MapTypeId.ROADMAP',
 '  };',
 '  map = new google.maps.Map(document.getElementById("#REGION_ID#_map"),myOptions);',
+'  if (syncitem !== "") {',
+'    var val = $v(syncitem);',
+'    if (val !== null && val.indexOf(",") > -1) {',
+'      var arr = val.split(",");',
+'      console.log(''init from item ''+val);',
+'      var pos = new google.maps.LatLng(arr[0],arr[1]);',
+'      userpin = new google.maps.Marker({map: map, position: pos, icon: "#ICON#"});',
+'      setCircle(map,pos);',
+'    }',
+'    //if the lat/long item is changed, move the pin',
+'    $("#"+syncitem).change(function(){ ',
+'      var latlng = this.value;',
+'      console.log(''item changed ''+latlng);',
+'      if (latlng !== null && latlng !== undefined && latlng.indexOf(",") > -1) {',
+'        var arr = latlng.split(",");',
+'        userPin(map,arr[0],arr[1]);',
+'      }    ',
+'    });',
+'  }',
+'  if (distitem) {',
+'    //if the distance item is changed, redraw the circle',
+'    $("#"+distitem).change(function(){',
+'      console.log(''distitem changed ''+this.value);',
+'      var radius_metres = parseFloat(this.value)*1000;',
+'      if (distcircle.getRadius() !== radius_metres) {',
+'        distcircle.setRadius(radius_metres);',
+'      }',
+'    });',
+'  }',
 '  for (var i = 0; i < mapdata.length; i++) {',
-'    addMarker(map,mapdata[i]);',
+'    repPin(map,mapdata[i]);',
 '  }',
 '  map.fitBounds(bounds);',
+'  google.maps.event.addListener(map, ''click'', function (event) {',
+'    var lat = event.latLng.lat()',
+'       ,lng = event.latLng.lng();',
+'    console.log(''map clicked ''+lat+'',''+lng);',
+'    userPin(map,lat,lng);',
+'    if ("#SYNCITEM#" !== "") {',
+'      $s("#SYNCITEM#",lat+","+lng);',
+'    }',
+'    apex.jQuery("##REGION_ID#").trigger("mapclick", {lat:lat, lng:lng});',
+'  });',
+'  console.log(''initMap finished'');',
 '}',
 'window.onload = function() {',
 '  mapdata = [#MAPDATA#];',
@@ -172,25 +301,32 @@ wwv_flow_api.create_plugin(
 '</script>',
 '<div id="#REGION_ID#_map" style="min-height:#MAPHEIGHT#px"></div>]'';',
 '',
-'    l_html := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(l_html',
+'    l_html := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(',
+'              REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(',
+'              REPLACE(',
+'      l_html',
 '      ,''#SOUTHWEST#'', l_southwest)',
 '      ,''#NORTHEAST#'', l_northeast)',
 '      ,''#MAPDATA#'',   l_markers_data)',
 '      ,''#MAPHEIGHT#'', l_map_height)',
-'      ,''#ITEMNAME#'',  l_item_name)',
+'      ,''#IDITEM#'',    l_id_item)',
 '      ,''#CLICKZOOM#'', l_click_zoom)',
 '      ,''#REGION_ID#'', CASE',
 '                      WHEN p_region.static_id IS NOT NULL',
 '                      THEN p_region.static_id',
 '                      ELSE ''R''||p_region.id',
-'                      END);',
+'                      END)',
+'      ,''#LATLNG#'',    l_latlong)',
+'      ,''#SYNCITEM#'',  l_sync_item)',
+'      ,''#ICON#'',      l_markericon)',
+'      ,''#DISTITEM#'',  l_dist_item);',
 '      ',
 '    sys.htp.p(l_html);',
 '  ',
 '    return l_result;',
-'END render_map;    '))
+'END render_map;'))
 ,p_render_function=>'render_map'
-,p_standard_attributes=>'SOURCE_SQL:SOURCE_REQUIRED:AJAX_ITEMS_TO_SUBMIT'
+,p_standard_attributes=>'SOURCE_SQL:AJAX_ITEMS_TO_SUBMIT'
 ,p_sql_min_column_count=>5
 ,p_sql_max_column_count=>6
 ,p_sql_examples=>wwv_flow_utilities.join(wwv_flow_t_varchar2(
@@ -221,7 +357,7 @@ wwv_flow_api.create_plugin(
 'http://maps.google.com/mapfiles/ms/icons/pink-pushpin.png',
 'http://maps.google.com/mapfiles/ms/icons/purple-pushpin.png',
 'http://maps.google.com/mapfiles/ms/icons/red-pushpin.png'))
-,p_version_identifier=>'0.2'
+,p_version_identifier=>'0.3'
 ,p_about_url=>'https://github.com/jeffreykemp/jk64-plugin-reportmap'
 );
 wwv_flow_api.create_plugin_attribute(
@@ -235,7 +371,6 @@ wwv_flow_api.create_plugin_attribute(
 ,p_is_required=>true
 ,p_default_value=>'400'
 ,p_unit=>'pixels'
-,p_supported_ui_types=>'DESKTOP'
 ,p_is_translatable=>false
 ,p_help_text=>'Desired height (in pixels) of the map region. Note: the width will adjust according to the available area of the containing window.'
 );
@@ -248,7 +383,6 @@ wwv_flow_api.create_plugin_attribute(
 ,p_prompt=>'Set Item Name to ID on Click'
 ,p_attribute_type=>'PAGE ITEM'
 ,p_is_required=>false
-,p_supported_ui_types=>'DESKTOP'
 ,p_is_translatable=>false
 ,p_help_text=>'When the user clicks on a map marker, the corresponding ID from your data will be copied to this page item.'
 );
@@ -263,9 +397,70 @@ wwv_flow_api.create_plugin_attribute(
 ,p_is_required=>false
 ,p_default_value=>'13'
 ,p_unit=>'(0-23)'
-,p_supported_ui_types=>'DESKTOP'
 ,p_is_translatable=>false
-,p_help_text=>'When the user clicks on a map marker, zoom the map to this level. Set to blank to not zoom on click.'
+,p_help_text=>'When the user clicks on a map marker, or adds a new marker, zoom the map to this level. Set to blank to not zoom on click.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(73399822275640057)
+,p_plugin_id=>wwv_flow_api.id(68313922569222353)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>4
+,p_display_sequence=>40
+,p_prompt=>'Synchronize with Item'
+,p_attribute_type=>'PAGE ITEM'
+,p_is_required=>false
+,p_is_translatable=>false
+,p_help_text=>'Position of the marker will be retrieved from and stored in this item as a Lat,Long value.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(73403134287686512)
+,p_plugin_id=>wwv_flow_api.id(68313922569222353)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>5
+,p_display_sequence=>50
+,p_prompt=>'Marker Icon'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_is_translatable=>false
+,p_examples=>wwv_flow_utilities.join(wwv_flow_t_varchar2(
+'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+'http://maps.google.com/mapfiles/ms/icons/purple-dot.png',
+'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+'http://maps.google.com/mapfiles/ms/icons/ylw-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/blue-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/grn-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/ltblu-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/pink-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/purple-pushpin.png',
+'http://maps.google.com/mapfiles/ms/icons/red-pushpin.png'))
+,p_help_text=>'URL to the icon to show for the marker. Leave blank for the default red Google pin.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(73409755677475569)
+,p_plugin_id=>wwv_flow_api.id(68313922569222353)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>6
+,p_display_sequence=>60
+,p_prompt=>'Initial Map Position'
+,p_attribute_type=>'TEXT'
+,p_is_required=>false
+,p_unit=>'lat,long'
+,p_is_translatable=>false
+,p_help_text=>'Set the latitude and longitude as a pair of numbers to be used to position the map on page load, if no pin coordinates have been provided by the page item.'
+);
+wwv_flow_api.create_plugin_attribute(
+ p_id=>wwv_flow_api.id(73412118988175863)
+,p_plugin_id=>wwv_flow_api.id(68313922569222353)
+,p_attribute_scope=>'COMPONENT'
+,p_attribute_sequence=>7
+,p_display_sequence=>70
+,p_prompt=>'Circle Radius Item'
+,p_attribute_type=>'PAGE ITEM'
+,p_is_required=>false
+,p_is_translatable=>false
+,p_help_text=>'Set to an item which contains the distance (in Kilometres) to draw a circle around the click point. Leave blank to not draw a circle. If the item is changed, the circle will be updated.'
 );
 wwv_flow_api.create_plugin_event(
  p_id=>wwv_flow_api.id(68314239141225636)
--- render_map.sql
+++ render_map.sql
@@ -1,3 +1,16 @@
+set define off
+
+create or replace package plugin as
+function render_map (
+    p_region in apex_plugin.t_region,
+    p_plugin in apex_plugin.t_plugin,
+    p_is_printer_friendly in boolean )
+return apex_plugin.t_region_render_result;
+end plugin;
+/
+show errors
+
+create or replace package body plugin as
 function render_map (
     p_region in apex_plugin.t_region,
     p_plugin in apex_plugin.t_plugin,
@@ -24,8 +37,20 @@ as
 
     -- Component attributes
     l_map_height    plugin_attr := p_region.attribute_01;
-    l_item_name     plugin_attr := p_region.attribute_02;
+    l_id_item       plugin_attr := p_region.attribute_02;
     l_click_zoom    plugin_attr := p_region.attribute_03;    
+    l_sync_item     plugin_attr := p_region.attribute_04;
+    l_markericon    plugin_attr := p_region.attribute_05;
+    l_latlong       plugin_attr := p_region.attribute_06;
+    l_dist_item     plugin_attr := p_region.attribute_07;
+    
+    PROCEDURE set_map_extents (p_lat IN NUMBER, p_lng IN NUMBER) IS
+    BEGIN
+        l_lat_min := LEAST   (NVL(l_lat_min, p_lat), p_lat);
+        l_lat_max := GREATEST(NVL(l_lat_max, p_lat), p_lat);
+        l_lng_min := LEAST   (NVL(l_lng_min, p_lng), p_lng);
+        l_lng_max := GREATEST(NVL(l_lng_max, p_lng), p_lng);
+    END set_map_extents;
 BEGIN
     -- debug information will be included
     if apex_application.g_debug then
@@ -34,7 +59,7 @@ BEGIN
             p_region => p_region,
             p_is_printer_friendly => p_is_printer_friendly);
     end if;
-    
+
     IF p_region.source IS NOT NULL THEN
 
         l_column_value_list := apex_plugin_util.get_data
@@ -66,21 +91,38 @@ BEGIN
               || ',icon:' || APEX_ESCAPE.js_literal(l_icon)
               || '}';
         
-            l_lat_min := LEAST   (NVL(l_lat_min, l_lat), l_lat);
-            l_lat_max := GREATEST(NVL(l_lat_max, l_lat), l_lat);
-            l_lng_min := LEAST   (NVL(l_lng_min, l_lng), l_lng);
-            l_lng_max := GREATEST(NVL(l_lng_max, l_lng), l_lng);
+            set_map_extents (p_lat => l_lat, p_lng => l_lng);
           
         END LOOP;
         
     END IF;
     
+    l_lat := NULL;
+    l_lng := NULL;
+    
+    -- if sync item is set, include its position in the initial map extent
+    IF l_sync_item IS NOT NULL THEN
+      l_latlong := NVL(v(l_sync_item),l_latlong);
+    END IF;
+    
+    IF l_latlong IS NOT NULL THEN
+      l_lat := TO_NUMBER(SUBSTR(l_latlong,1,INSTR(l_latlong,',')-1));
+      l_lng := TO_NUMBER(SUBSTR(l_latlong,INSTR(l_latlong,',')+1));
+    END IF;
+    
+    IF l_lat IS NOT NULL THEN
+      set_map_extents (p_lat => l_lat, p_lng => l_lng);
+
     -- show entire map if no points to show
-    IF l_markers_data IS NULL THEN
+    ELSIF l_markers_data IS NULL THEN
+      l_lat := 0;
+      l_lng := 0;
+      l_latlong := '0,0';
       l_lat_min := -90;
       l_lat_max := 90;
       l_lng_min := -180;
       l_lng_max := 180;
+
     END IF;
     
     l_southwest := '{lat:'||TO_CHAR(l_lat_min)||',lng:'||TO_CHAR(l_lng_min)||'}';
@@ -88,16 +130,16 @@ BEGIN
     
     l_html := q'[
 <script>
-var map, iw, marker, mapdata;
-function addMarker(map,pData) {
-  marker = new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(pData.lat, pData.lng),
-                title: pData.name,
-                icon: pData.icon
-            });
- 
-  google.maps.event.addListener(marker, 'click', function () {
+var map, iw, reppin, userpin, distcircle, mapdata;
+function repPin(map,pData) {
+  var reppin = new google.maps.Marker({
+                 map: map,
+                 position: new google.maps.LatLng(pData.lat, pData.lng),
+                 title: pData.name,
+                 icon: pData.icon
+               }); 
+  google.maps.event.addListener(reppin, 'click', function () {
+    console.log('reppin clicked '+pData.id);
     if (iw) {
       iw.close();
     } else {
@@ -111,22 +153,122 @@ function addMarker(map,pData) {
     if ("#CLICKZOOM#" != "") {
       map.setZoom(#CLICKZOOM#);
     }
-    if ("#ITEMNAME#" !== "") {
-      $s("#ITEMNAME#",pData.id);
+    if ("#IDITEM#" !== "") {
+      $s("#IDITEM#",pData.id);
     }
-    apex.jQuery("##REGION_ID#").trigger("mapclick", {id:pData.id, name:pData.name, lat:pData.lat, lng:pData.lng});
+    apex.jQuery("##REGION_ID#").trigger("markerclick", {id:pData.id, name:pData.name, lat:pData.lat, lng:pData.lng});
   });
 }
+function setCircle(map,pos) {
+  var distitem = "#DISTITEM#";
+  if (distitem!=="") {
+    if (distcircle) {
+      console.log('move circle');
+      distcircle.setCenter(pos);
+    } else {
+      console.log('create circle');
+      var radius_metres = parseFloat($v(distitem))*1000;
+      distcircle = new google.maps.Circle({
+          strokeColor: '#5050FF',
+          strokeOpacity: 0.5,
+          strokeWeight: 2,
+          fillColor: '#0000FF',
+          fillOpacity: 0.05,
+          clickable: false,
+          editable: true,
+          map: map,
+          center: pos,
+          radius: radius_metres
+        });
+      google.maps.event.addListener(distcircle, 'radius_changed', function (event) {
+        var km = distcircle.getRadius()/1000;
+        console.log('circle radius changed '+km);
+        $s("#DISTITEM#", km);
+      });
+    }
+  }
+}
+function userPin(map,lat,lng) {
+  if (lat !== null && lng !== null) {
+    var oldpos = userpin?userpin.getPosition():new google.maps.LatLng(0,0);
+    if (lat == oldpos.lat() && lng == oldpos.lng()) {
+      console.log('not changed');
+    } else {
+      var pos = new google.maps.LatLng(lat,lng);
+      if (userpin) {
+        console.log('move existing pin to new position on map '+lat+','+lng);
+        userpin.setMap(map);
+        userpin.setPosition(pos);
+        setCircle(map,pos);
+      } else {
+        console.log('create userpin '+lat+','+lng);
+        userpin = new google.maps.Marker({map: map, position: pos, icon: "#ICON#"});
+        setCircle(map,pos);
+      }
+    }
+  } else if (userpin) {
+    console.log('move existing pin off the map');
+    userpin.setMap(null);
+    if (distcircle) {
+      distcircle.setMap(null);
+    }
+  }
+}
 function initMap() {
-  var bounds = new google.maps.LatLngBounds(#SOUTHWEST#,#NORTHEAST#);
+  console.log('initMap');
+  var syncitem = "#SYNCITEM#"
+     ,distitem = "#DISTITEM#"
+     ,bounds = new google.maps.LatLngBounds(#SOUTHWEST#,#NORTHEAST#);
   var myOptions = {
+    zoom: 1,
+    center: new google.maps.LatLng(#LATLNG#),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("#REGION_ID#_map"),myOptions);
+  if (syncitem !== "") {
+    var val = $v(syncitem);
+    if (val !== null && val.indexOf(",") > -1) {
+      var arr = val.split(",");
+      console.log('init from item '+val);
+      var pos = new google.maps.LatLng(arr[0],arr[1]);
+      userpin = new google.maps.Marker({map: map, position: pos, icon: "#ICON#"});
+      setCircle(map,pos);
+    }
+    //if the lat/long item is changed, move the pin
+    $("#"+syncitem).change(function(){ 
+      var latlng = this.value;
+      console.log('item changed '+latlng);
+      if (latlng !== null && latlng !== undefined && latlng.indexOf(",") > -1) {
+        var arr = latlng.split(",");
+        userPin(map,arr[0],arr[1]);
+      }    
+    });
+  }
+  if (distitem) {
+    //if the distance item is changed, redraw the circle
+    $("#"+distitem).change(function(){
+      console.log('distitem changed '+this.value);
+      var radius_metres = parseFloat(this.value)*1000;
+      if (distcircle.getRadius() !== radius_metres) {
+        distcircle.setRadius(radius_metres);
+      }
+    });
+  }
   for (var i = 0; i < mapdata.length; i++) {
-    addMarker(map,mapdata[i]);
+    repPin(map,mapdata[i]);
   }
   map.fitBounds(bounds);
+  google.maps.event.addListener(map, 'click', function (event) {
+    var lat = event.latLng.lat()
+       ,lng = event.latLng.lng();
+    console.log('map clicked '+lat+','+lng);
+    userPin(map,lat,lng);
+    if ("#SYNCITEM#" !== "") {
+      $s("#SYNCITEM#",lat+","+lng);
+    }
+    apex.jQuery("##REGION_ID#").trigger("mapclick", {lat:lat, lng:lng});
+  });
+  console.log('initMap finished');
 }
 window.onload = function() {
   mapdata = [#MAPDATA#];
@@ -135,20 +277,30 @@ window.onload = function() {
 </script>
 <div id="#REGION_ID#_map" style="min-height:#MAPHEIGHT#px"></div>]';
 
-    l_html := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(l_html
+    l_html := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+              REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+              REPLACE(
+      l_html
       ,'#SOUTHWEST#', l_southwest)
       ,'#NORTHEAST#', l_northeast)
       ,'#MAPDATA#',   l_markers_data)
       ,'#MAPHEIGHT#', l_map_height)
-      ,'#ITEMNAME#',  l_item_name)
+      ,'#IDITEM#',    l_id_item)
       ,'#CLICKZOOM#', l_click_zoom)
       ,'#REGION_ID#', CASE
                       WHEN p_region.static_id IS NOT NULL
                       THEN p_region.static_id
                       ELSE 'R'||p_region.id
-                      END);
+                      END)
+      ,'#LATLNG#',    l_latlong)
+      ,'#SYNCITEM#',  l_sync_item)
+      ,'#ICON#',      l_markericon)
+      ,'#DISTITEM#',  l_dist_item);
       
     sys.htp.p(l_html);
   
     return l_result;
-END render_map;    
+END render_map;
+end plugin;
+/
+show errors
